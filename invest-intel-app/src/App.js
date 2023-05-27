dabba2b873abfad01088f5ecf673250b88ba664e
soumyamund <soumyaranjan.nitr@gmail.com>
2023-05-28 00:10:41 +0530

    Static list pipulation of stock

--- invest-intel-app/src/App.js
+++ invest-intel-app/src/App.js
@@ -1,15 +1,56 @@
-const name = "Srm"
-function getTitle(title) {
-  return title;
+const stockList = [
+  {
+    id: 1,
+    symbol: "INFY",
+    price: 1322.0,
+    lotsize: 400,
+  },
+
+  {
+    id: 2,
+    symbol: "HDFCBANK",
+    price: 1122.0,
+    lotsize: 250,
+  },
+];
+
+function getLotPrice (lotSize, price) {
+  return lotSize*price
+}
+
+function getValuation(lotPrice) {
+  if(lotPrice<500000) return 'LOW'
+  if(lotPrice<700000) return 'MID'
+  else return 'HIGH'
 }
 
 function App() {
   return (
     <div>
-      <h1>"Hello {getTitle(name)}"</h1>
-      <label htmlFor="search">Search:</label>
-      <input id="search" type="text"/>
-     </div>
+      <h1>Stock List</h1> 
+      <hr />
+      {stockList.map(function (stock) {
+        return (
+          <div key={stock.price}>
+            <span>
+              {stock.symbol} : {stock.price}
+            </span>
+            <br />
+            <span>lotsize : {stock.lotsize}</span>
+            <br />
+            <span>
+              LotPrice: {getLotPrice(stock.lotsize,  stock.price)}
+            </span>
+            <br />
+            <span>
+              Valuation: {getValuation(getLotPrice(stock.lotsize,  stock.price))}
+            </span>
+            <br />
+            <hr />
+          </div>
+        );
+      })}
+    </div>
   );
 }
 
