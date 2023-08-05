39e8e272a5645f910248d6c76a031d78a1f71bac
soumyamund <soumyaranjan.nitr@gmail.com>
2023-08-06 04:26:13 +0530

    Fetch API data

--- invest-intel-app/src/App.js
+++ invest-intel-app/src/App.js
@@ -1,117 +1,113 @@
 import React from "react";
 
-const stockList = [
+const url =
+  "https://twelve-data1.p.rapidapi.com/stocks?exchange=NASDAQ&format=json";
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "9c342da74bmsh13ce343e2934570p1db565jsn5c628f1c01c8",
+    "X-RapidAPI-Host": "twelve-data1.p.rapidapi.com",
+  },
+};
+
+const initialStocks = [
   {
     id: 1,
-    symbol: "INFY",
-    price: 1322.0,
-    lotsize: 400,
+    symbol: "AACG",
+    name: "ATA Creativity Global",
+    currency: "USD",
+    exchange: "NASDAQ",
+    mic_code: "XNMS",
+    country: "United States",
+    type: "Depositary Receipt",
   },
 
   {
     id: 2,
-    symbol: "HDFCBANK",
-    price: 1122.0,
-    lotsize: 250,
+    symbol: "AACI",
+    name: "Armada Acquisition Corp. I",
+    currency: "USD",
+    exchange: "NASDAQ",
+    mic_code: "XNMS",
+    country: "United States",
+    type: "Common Stock",
   },
 
   {
     id: 3,
-    symbol: "HCLTECH",
-    price: 1145.0,
-    lotsize: 250,
-  },
-
-  {
-    id: 4,
-    symbol: "RELIANCE",
-    price: 2000.0,
-    lotsize: 300,
-  },
-
-  {
-    id: 5,
-    symbol: "TCS",
-    price: 3300.0,
-    lotsize: 200,
-  },
-
-  {
-    id: 6,
-    symbol: "Asian Paints",
-    price: 3250.0,
-    lotsize: 150,
-  },
-
-  {
-    id: 7,
-    symbol: "WIPRO",
-    price: 409,
-    lotsize: 300,
-  },
-
-  {
-    id: 8,
-    symbol: "HDFC",
-    price: 2655,
-    lotsize: 300,
-  },
-
-  {
-    id: 9,
-    symbol: "Divis Lab",
-    price: 3554,
-    lotsize: 250,
-  },
-
-  {
-    id: 2,
-    symbol: "HDFC Life",
-    price: 586.0,
-    lotsize: 600,
+    symbol: "AACIU",
+    name: "Armada Acquisition Corp. I Unit",
+    currency: "USD",
+    exchange: "NASDAQ",
+    mic_code: "XNMS",
+    country: "United States",
+    type: "Common Stock",
   },
 ];
-
-const getLotPrice = (lotSize, price) =>  lotSize * price;
-
-const getValuation = (lotPrice) => {
-  if (lotPrice < 500000) return "LOW";
-  if (lotPrice < 700000) return "MID";
-  else return "HIGH";
+const useSemiPersistentState = (key, initialState) => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [key, value]);
+  return [value, setValue];
 };
 
-const useSemiPersistentState = (key,initialState) => {
-  const [value, setValue] = React.useState(localStorage.getItem(key)||initialState)
-  React.useEffect(()=>{
-    localStorage.setItem(key, value)
-  },[key,value])
-  return [value, setValue];
-}
+/* const getAsyncStockData = async () => {
+
+  try {
+    const response = await fetch(url, options);
+    const result = await response.text();
+    return result
+  } catch (error) {
+    console.error(error);
+  }
+} */
 
 const App = () => {
-  const [searchTerm, setSearchTerm] = useSemiPersistentState('search', '')
+  const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "");
+
   const handleSearch = (event) => {
     console.log(event);
     setSearchTerm(event.target.value);
   };
-  const filteredStockList = stockList.filter(stock => stock.symbol.toLowerCase().includes(searchTerm.toLowerCase()));
 
+  const [stocks, setStocks] = React.useState(initialStocks);
+
+  React.useEffect(() => {
+    const promiseResponse = fetch(url, options);
+    //debugger;
+    const promiseResult = promiseResponse.then((response) => {
+      //debugger;
+      return response.text();
+    });
+    promiseResult.then((result) => {
+      //debugger;
+      const jsonData = JSON.parse(result);
+      setStocks(jsonData.data);
+    });
+  }, []);
+
+  const filteredStocks = stocks.filter((stock) => {
+    return (
+      stock.symbol.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      stock.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  });
   return (
     <div>
       <h1>Stock List</h1>
-      <SearchBar
-        searchTerm={searchTerm}
-        onSearch={handleSearch}
-      />
+      <SearchBar searchTerm={searchTerm} onSearch={handleSearch} />
       <SearchTitle searchTerm={searchTerm} />
       <hr />
-      <ListView list={filteredStockList} />
+      <ListView list={filteredStocks} />
     </div>
   );
 };
 
 const SearchTitle = (props) => {
-  const strLen = props.searchTerm.trim().length
+  const strLen = props.searchTerm.trim().length;
   if (strLen) {
     return (
       <p>
@@ -136,17 +132,15 @@ const SearchBar = ({ searchTerm, onSearch }) => (
   </>
 );
 
-const Item = ({ symbol, lotsize, price }) => (
+const Item = ({ symbol, name, currency, exchange }) => (
   <div>
-    <span>
-      {symbol} : {price}
-    </span>
+    <span>symbol : {symbol}</span>
     <br />
-    <span>lotsize : {lotsize}</span>
+    <span>name : {name}</span>
     <br />
-    <span>LotPrice: {getLotPrice(lotsize, price)}</span>
+    <span>currency: {currency}</span>
     <br />
-    <span>Valuation: {getValuation(getLotPrice(lotsize, price))}</span>
+    <span>exchange: {exchange}</span>
     <br />
   </div>
 );
@@ -158,6 +152,5 @@ const ListView = ({ list }) =>
       <hr />
     </div>
   ));
-
 
 export default App;
