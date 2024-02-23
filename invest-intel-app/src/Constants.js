be0478a806b34364ff71e24fce803526ee0704c2
soumyamund <soumyaranjan.nitr@gmail.com>
2024-02-23 17:14:16 +0530

    Breaking front end change to handle API formatting change

---
+++ invest-intel-app/src/Constants.js
@@ -0,0 +1,5 @@
+export const SYMBOL = "SYMBOL";
+export const PRICE = "LTP";
+export const YEAR_HIGH = "52WH";
+export const CHANGE_PERCENT = "%CHNG";
+export const YEAR_LOW = "52WL";
--- invest-intel-app/src/Stock.js
+++ invest-intel-app/src/Stock.js
@@ -1,47 +1,53 @@
 import React from "react";
 
-import './App.css';
+import "./App.css";
+import {
+  SYMBOL,
+  PRICE,
+  CHANGE_PERCENT,
+  YEAR_HIGH,
+  YEAR_LOW,
+} from "./Constants";
 
 const url =
   "https://investinteldatatrigger.azurewebsites.net/api/InvestIntelDataTrigger";
 const options = {
-  method: "GET"
+  method: "GET",
 };
 
 const initialStocks = [
-    {
-      "SYMBOL ": "NIFTY 50",
-      "OPEN ": "21,727.00",
-      "HIGH ": "21,804.45",
-      "LOW ": "21,629.90",
-      "PREV. CLOSE ": "21,717.95",
-      "LTP ": "21,782.50",
-      "CHNG ": "64.55",
-      "%CHNG ": "0.30",
-      "VOLUME (shares)": "34,92,22,850",
-      "VALUE  (₹ Crores)": "31,175.97",
-      "52W H ": "22,126.80",
-      "52W L ": "16,828.35",
-      "30 D   %CHNG ": "0.76",
-      "365 D % CHNG  09-Feb-2023": "21.73"
-    },
-
-    {
-      "SYMBOL ": "GRASIM",
-      "OPEN ": "2,057.25",
-      "HIGH ": "2,182.00",
-      "LOW ": "2,046.90",
-      "PREV. CLOSE ": "2,057.30",
-      "LTP ": "2,179.05",
-      "CHNG ": "121.75",
-      "%CHNG ": "5.92",
-      "VOLUME (shares)": "20,10,089",
-      "VALUE  (₹ Crores)": "428.45",
-      "52W H ": "2,182.05",
-      "52W L ": "1,527.05",
-      "30 D   %CHNG ": "4.83",
-      "365 D % CHNG  09-Feb-2023": "31.95"
-    }
+  {
+    SYMBOL: "NIFTY 50",
+    OPEN: "22,290.00",
+    HIGH: "22,297.50",
+    LOW: "22,186.10",
+    "PREV.CLOSE": "22,217.45",
+    LTP: "22,212.70",
+    CHNG: "-4.75",
+    "%CHNG": "-0.02",
+    "VOLUME(shares)": "22,59,83,194",
+    "VALUE(₹Crores)": "24,616.91",
+    "52WH": "22,297.50",
+    "52WL": "16,828.35",
+    "30D%CHNG": "4.61",
+    "365D%CHNG22-Feb-2023": "26.56",
+  },
+  {
+    SYMBOL: "BAJAJFINSV",
+    OPEN: "1,596.00",
+    HIGH: "1,620.90",
+    LOW: "1,596.00",
+    "PREV.CLOSE": "1,592.55",
+    LTP: "1,616.00",
+    CHNG: "23.45",
+    "%CHNG": "1.47",
+    "VOLUME(shares)": "11,99,419",
+    "VALUE(₹Crores)": "193.14",
+    "52WH": "1,741.00",
+    "52WL": "1,215.00",
+    "30D%CHNG": "0.64",
+    "365D%CHNG22-Feb-2023": "17.02",
+  },
 ];
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = React.useState(
@@ -52,7 +58,6 @@ const useSemiPersistentState = (key, initialState) => {
   }, [key, value]);
   return [value, setValue];
 };
-
 
 const Stock = () => {
   const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "");
@@ -79,9 +84,7 @@ const Stock = () => {
   }, []);
 
   const filteredStocks = stocks.filter((stock) => {
-    return (
-      stock["SYMBOL "].toLowerCase().includes(searchTerm.toLowerCase())
-    );
+    return stock[SYMBOL].toLowerCase().includes(searchTerm.toLowerCase());
   });
   return (
     <div>
@@ -108,53 +111,53 @@ const SearchTitle = (props) => {
 
 const SearchBar = ({ searchTerm, onSearch }) => (
   <>
-  <div className="search-container">
-    <input className="search-input" 
-      id="search"
-      type="text"
-      placeholder="Search Stock..."
-      value={searchTerm}
-      onChange={onSearch}
-    ></input>
-    <button className="search-button" type="submit">Search</button>
+    <div className="search-container">
+      <input
+        className="search-input"
+        id="search"
+        type="text"
+        placeholder="Search Stock..."
+        value={searchTerm}
+        onChange={onSearch}
+      ></input>
+      <button className="search-button" type="submit">
+        Search
+      </button>
     </div>
   </>
 );
 
 const numberWithOutComma = (num) => {
-   return parseFloat(num.replace(/,/g, ''));
-}
+  return parseFloat(num.replace(/,/g, ""));
+};
 
+function Item(stock) {
+  const low = numberWithOutComma(stock[YEAR_LOW]);
+  const high = numberWithOutComma(stock[YEAR_HIGH]);
+  const price = numberWithOutComma(stock[PRICE]);
+  const price_pos = Math.round(((price - low) / (high - low)) * 100);
 
-function Item (stock) { 
-  const low = numberWithOutComma(stock["52W L "]);
-  const high = numberWithOutComma(stock["52W H "]);
-  const price = numberWithOutComma(stock["LTP "]);
-  const price_pos = Math.round((price - low)/(high-low) * 100);
-  
-  const circleposStyle = { 
-    left : `${price_pos}%`
+  const circleposStyle = {
+    left: `${price_pos}%`,
   };
-  return ( 
-  <div className={`card ${stock["%CHNG "] > 0 ? 'green' : 'red'}`}>
-    <div className="left-section">
-      <h2>{stock["SYMBOL "]}</h2>
-    </div>
-    <div className="middle-section">
-      <p>price : {price}</p>
-      <p>1Y Low : {low}</p>
-      <p>1Y High : {high}</p>
-    </div>
-    <div className="right-section">
-      <div className="graph-line">
-      <div className="graph-circle" style={circleposStyle}></div>
+  return (
+    <div className={`card ${stock[CHANGE_PERCENT] > 0 ? "green" : "red"}`}>
+      <div className="left-section">
+        <h2>{stock[SYMBOL]}</h2>
+      </div>
+      <div className="middle-section">
+        <p>price : {price}</p>
+        <p>1Y Low : {low}</p>
+        <p>1Y High : {high}</p>
+      </div>
+      <div className="right-section">
+        <div className="graph-line">
+          <div className="graph-circle" style={circleposStyle}></div>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
 }
-
-
 
 const ListView = ({ list }) =>
   list.map((item, index) => (
