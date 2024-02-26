50dd9efe44272afcab138e7b26ef19c845859b55
soumyamund <soumyaranjan.nitr@gmail.com>
2024-02-26 19:46:02 +0530

    More UI modification

--- invest-intel-app/src/StockCard.js
+++ invest-intel-app/src/StockCard.js
@@ -12,11 +12,7 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import UpIcon from "./icons/uptrend.svg";
 import DownIcon from "./icons/downtrend.svg";
 import Utils from "./Utils";
-import {
-  PRICE,
-  YEAR_HIGH,
-  YEAR_LOW,
-} from "./Constants";
+import { PRICE, YEAR_HIGH, YEAR_LOW } from "./Constants";
 
 import PercentageLine from "./PercentageLine";
 const theme = createTheme();
@@ -27,15 +23,22 @@ function StockCard(stock) {
   const low = Utils.numberWithOutComma(stock[YEAR_LOW]);
   const high = Utils.numberWithOutComma(stock[YEAR_HIGH]);
   const price = Utils.numberWithOutComma(stock[PRICE]);
-  const price_pos = Math.round(((price - low) / (high - low)) *100);
-  console.log(price_pos);
+  const price_pos = Math.round(((price - low) / (high - low)) * 100);
+  const color_subheader = positive_daily > 0 ? "green" : "red";
 
   return (
     <ThemeProvider theme={theme}>
       <Card>
         <CardHeader
           title={stock.SYMBOL}
-          subheader={stock.LTP}
+          subheader={
+            <span>
+              {stock.LTP}
+              <Typography variant="body2" color={color_subheader}>
+                {stock.CHNG} ({stock["%CHNG"]}%)
+              </Typography>
+            </span>
+          }
           avatar={
             <Avatar>
               <img src={stock.CHNG > 0 ? UpIcon : DownIcon} alt="Trend Icon" />
@@ -49,7 +52,7 @@ function StockCard(stock) {
             direction="row"
             justifyContent="space-around"
           >
-            <Grid item xs={3} sm container>
+            <Grid item xs={6} sm container>
               <Grid item xs container direction="column">
                 <Typography variant="body2" gutterBottom>
                   low: {stock.LOW}
@@ -57,33 +60,14 @@ function StockCard(stock) {
                 <Typography variant="body2" gutterBottom>
                   high: {stock.HIGH}
                 </Typography>
-                <Typography variant="body2" gutterBottom>
-                  52w Year low: {stock["52WL"]}
-                </Typography>
-                <Typography variant="body2" gutterBottom>
-                  52w Year high: {stock["52WH"]}
-                </Typography>
               </Grid>
             </Grid>
-            <Grid item xs={3} sm container>
+            <Grid item xs={6} sm container>
               <Grid item xs container direction="column">
                 <Typography variant="body2" gutterBottom>
                   prev: {stock["PREV.CLOSE"]}
                 </Typography>
-                <Typography
-                  variant="body2"
-                  gutterBottom
-                  sx={{ color: positive_daily ? "green" : "red" }}
-                >
-                  change: {stock.CHNG}
-                </Typography>
-                <Typography
-                  variant="body2"
-                  gutterBottom
-                  sx={{ color: positive_daily ? "green" : "red" }}
-                >
-                  change %: {stock["%CHNG"]}
-                </Typography>
+
                 <Typography
                   variant="body2"
                   gutterBottom
@@ -93,9 +77,9 @@ function StockCard(stock) {
                 </Typography>
               </Grid>
             </Grid>
-            <Grid item xs={3} sm container>
+            <Grid item xs={6} sm container>
               <Grid item xs container direction="column">
-                <Typography variant="button" gutterBottom>
+                <Typography variant="body2" gutterBottom>
                   lot price:
                 </Typography>
                 <Typography variant="body2" gutterBottom>
@@ -106,8 +90,14 @@ function StockCard(stock) {
                 </Typography>
               </Grid>
             </Grid>
-            <Grid item xs={3} sm container gridRow={12} alignContent= "center">
+            <Grid item xs={6} sm container gridRow={12} alignContent="center">
               <Grid item xs container direction="column" spacing={2}>
+                <Typography variant="body2" gutterBottom>
+                  52W low: {stock["52WL"]}
+                </Typography>
+                <Typography variant="body2" gutterBottom>
+                  52W high: {stock["52WH"]}
+                </Typography>
                 <PercentageLine percentage={price_pos}></PercentageLine>
               </Grid>
             </Grid>
