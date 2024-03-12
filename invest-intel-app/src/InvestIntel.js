e3412c376b936d4507fd0b45373d161045ac12cb
soumyamund <soumyaranjan.nitr@gmail.com>
2024-03-12 12:34:41 +0530

    Make search bar visible till redesign is done

--- invest-intel-app/src/InvestIntel.js
+++ invest-intel-app/src/InvestIntel.js
@@ -6,6 +6,7 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Stock from "./Stock";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ComingSoon from "./ComingSoon";
+import { Box } from "@mui/material";
 
 const sections = [
   { title: "Stock", url: "stocks" },
@@ -13,22 +14,26 @@ const sections = [
   { title: "Task", url: "task" },
 ];
 
+const mytheme = createTheme();
+
 const InvestIntel = () => {
   return (
-    <ThemeProvider theme={createTheme()}>
+    <ThemeProvider theme={mytheme}>
       <CssBaseline />
       <CssBaseline />
       <Container maxWidth="lg">
         <FixedWidthAppBar title="Invest-Intel" sections={sections} />
       </Container>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Stock />} />
-          <Route path="/stocks" element={<Stock />} />
-          <Route path="/health" element={<ComingSoon />} />
-          <Route path="/task" element={<ComingSoon />} />
-        </Routes>
-      </Router>
+      <Box style={{ marginTop: "70px", width: "100%", padding: 40 }}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Stock />} />
+            <Route path="/stocks" element={<Stock />} />
+            <Route path="/health" element={<ComingSoon />} />
+            <Route path="/task" element={<ComingSoon />} />
+          </Routes>
+        </Router>
+      </Box>
     </ThemeProvider>
   );
 };
